be8ee72885e5e943936e5d2ee4f0635489f3cf15
TheBrenny <iam@justbrenny.me>
2023-01-15 13:48:24 +1100

    ts whinged?

--- src/js/forks.d.ts
+++ src/js/forks.d.ts
@@ -1,17 +1,17 @@
 export interface ForkSettings extends ForkReuseSettings {
     name: String;
-};
+}
 
 export interface ForkReuseSettings {
     fn: String | Function;
-    args?: any[] = [];
-    messageHandler?: Function | null = null;
+    args?: any[];
+    messageHandler?: Function | null;
 }
 
 export class Fork {
     constructor(settings: ForkSettings);
     reuse(settings: ForkReuseSettings): boolean;
-    get promise(): Promise;
+    get promise(): Promise<any | undefined>;
     get retVal(): any;
     get retValCache(): any[];
     get isConnected(): boolean;
